d2dd216a30776cc08d6046b26ce740a8e02e90cf
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2021-11-21 16:53:32 -0500

    finished fixing the functionality of the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Tilt from 'react-vanilla-tilt';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+ const [calc, SetCalc] = useState('');
+ const [result, SetResult] = useState('');
+
+ const operators = ['/', '*', '+', '-', '.'];
+
+ const updateCalc = value => {
+  if (operators.includes(value) && calc === '' || operators.includes(value) && operators.includes(calc.slice(-1))) {
+   return;
+  }
+
+  SetCalc(calc + value);
+
+  if (!operators.includes(value)) {
+   SetResult(eval(calc + value).toString());
+  }
+ };
+
+ const createDigits = () => {
+  const digits = [];
+
+  for (let i = 1; i < 10; i++) {
+   digits.push(
+    <span className="num" key={i} onClick={() => updateCalc(i.toString())}>{i}</span>
+   );
+  }
+  return digits;
+ };
+
+ const calculate = () => {
+  if (calc == '') {
+   return;
+  } else {
+   SetCalc(eval(calc).toString());
+  }
+ };
+
+ const deleteLast = () => {
+  if (calc == '') {
+   return;
+  }
+
+  const value = calc.slice(0, -1);
+  SetCalc(value);
+ };
+
+ const clearInputField = () => {
+  SetCalc('');
+ };
+
+ const handleEnterKey = (event) => {
+  if (event.key == 'Enter') {
+   console.log('the key was pressed');
+   this.calculate();
+  }
+ };
+
+ return (
+  <Tilt className="tilt" options={{
+   scale: 2, max: 25, glare: true,
+   'max-glare': 1,
+  }} >
+   <div className="container">
+    <div className="calculator">
+     <div className="value">
+      {calc || "0"}
+     </div>
+     <span className="num" onClick={deleteLast}>DEL</span>
+     <span className="num" onClick={clearInputField}>CLEAR</span>
+     <span className="num" onClick={() => updateCalc('/')}>/</span>
+     <span className="num" onClick={() => updateCalc('*')}>*</span>
+     <span className="num" onClick={() => updateCalc('+')}>+</span>
+     <span className="num" onClick={() => updateCalc('-')}>-</span>
+     {createDigits()}
+     <span className="num" onClick={() => updateCalc('0')}>0</span>
+     <span className="num" onClick={() => updateCalc('.')}>.</span>
+     <span className="num" onClick={calculate}>=</span>
     </div>
-  );
+   </div>
+  </ Tilt>
+ );
 }
 
 export default App;
